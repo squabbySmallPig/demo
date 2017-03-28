e305394faaeeb5751bffc1f66a738aef87ff4820
squabbySmallPig <squabbySmallPig@163.com>
2017-03-28 16:53:09 +0800

    first commit:

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    receivedEvent: function() {
+    	var content = document.getElementById("box")
+        alert(content.getAttribute('data-text'));
+    }
+};
